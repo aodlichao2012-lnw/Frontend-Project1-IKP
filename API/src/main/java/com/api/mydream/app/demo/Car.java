b5cfd01ed1921f5e40f573ff46bf210dcc93155b
aodlichao2012-lnw <aodlichao2012@hotmail.com>
2019-08-22 11:38:55 +0700

    5555

---
+++ API/src/main/java/com/api/mydream/app/demo/Car.java
@@ -0,0 +1,17 @@
+package com.api.mydream.app.demo;
+
+import javax.persistence.Entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Car {
+
+}
