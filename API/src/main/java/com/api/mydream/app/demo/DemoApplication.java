9754eb903b948f59d62a8271a31ac4c9a3d86cf8
aodlichao2012-lnw <aodlichao2012@hotmail.com>
2019-08-22 15:36:23 +0700

    rrrr

--- API/src/main/java/com/api/mydream/app/demo/DemoApplication.java
+++ API/src/main/java/com/api/mydream/app/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import java.util.stream.Stream;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.api.mydream.app.demo.interfac.IRegisterParameter;
 
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
